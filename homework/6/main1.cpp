57ec3e69351f23acba47444076f173267b22b6fd
812610357 <63569510+812610357@users.noreply.github.com>
2020-04-22 13:08:54 +0800

    寻找最小数

--- homework/6/main1.cpp
+++ homework/6/main1.cpp
@@ -2,16 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Switch(int a[], int m, int n);
+
 int main()
 {
     int a[10] = {12, 23, -13, 56, -1, 14, 32, 57, 0, -20};
     int min, i;
     for (i = 1; i < 10; i++)
+    {
         if (a[i] < a[0])
-            a[0] = a[i];
+        {
+            Switch(a, i, 0);
+        }
+    }
     for (i = 0; i < 10; i++)
+    {
         printf("%d  ", a[i]);
+    }
     printf("\n最小的数字是%d\n", a[0]);
     system("pause");
     return 0;
+}
+
+void Switch(int a[], int m, int n)
+{
+    int s = a[m];
+    a[m] = a[n];
+    a[n] = s;
 }
--- homework/6/main2.cpp
+++ homework/6/main2.cpp
@@ -7,11 +7,19 @@ int main()
     int a[10] = {12, 23, -13, 56, -1, 14, 32, 57, 0, -20};
     int min, i, mini = 0;
     for (i = 1; i < 10; i++)
+    {
         if (a[i] < a[mini])
+        {
             mini = i;
+        }
+    }
+    int s = a[0];
     a[0] = a[mini];
+    a[mini] = s;
     for (i = 0; i < 10; i++)
+    {
         printf("%d  ", a[i]);
+    }
     printf("\n最小的数字是%d\n", a[0]);
     system("pause");
     return 0;
